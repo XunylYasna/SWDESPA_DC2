41c19d268338b2f12b160f6a986610d8218abb55
Lynux Ansay <lynux_ansay@dlsu.edu.ph>
2019-02-25 16:20:11 +0800

    Log in template

--- DesignChallenge2/src/Controllers/Controller.java
+++ DesignChallenge2/src/Controllers/Controller.java
@@ -1,4 +1,72 @@
 package Controllers;
 
-public class Controller {
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private VBox vbox;
+
+    private Parent fxml;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(vbox.getLayoutX() * 32);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("../Views/pageLogIn.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+
+    public void openLogin(ActionEvent actionEvent) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(vbox.getLayoutX() * 32);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("../Views/pageLogIn.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        });
+    }
+
+    public void openRegistration(ActionEvent actionEvent) {
+        TranslateTransition t = new TranslateTransition(Duration.seconds(1), vbox);
+        t.setToX(14);
+        t.play();
+        t.setOnFinished((e) -> {
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("../Views/pageRegistration.fxml"));
+                vbox.getChildren().removeAll();
+                vbox.getChildren().setAll(fxml);
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        });
+    }
 }
