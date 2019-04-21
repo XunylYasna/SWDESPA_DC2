97a97d674242b62c89f142681a738b7cf41ba696
Lynux Ansay <lynux_ansay@dlsu.edu.ph>
2019-04-22 02:10:58 +0800

    add albums

---
+++ DesignChallenge2/src/Database/AlbumAddHandler.java
@@ -0,0 +1,47 @@
+package Database;
+
+import Model.Playlist;
+import Model.Song;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+
+public class AlbumAddHandler {
+    PreparedStatement prepStatement;
+    Connection myConn = DatabaseConnection.getDatabaseConn();
+    Statement statement = null;
+    ResultSet resultSet;
+
+
+    public Boolean addAlbum(String albumName, File cover, String user) {
+
+        Boolean success = false;
+//      temp
+        String sql = "INSERT INTO album (albumname, songcover, artist)\n" +
+                "values (?, ?, ?)";// insert insert user query here
+        try {
+            prepStatement = myConn.prepareStatement(sql);
+            prepStatement.setString(1, albumName);
+            if(cover != null){
+                FileInputStream input = new FileInputStream(cover);
+                prepStatement.setBinaryStream(2,input);
+            }
+            else{
+                prepStatement.setBinaryStream(2,null);
+            }
+
+            prepStatement.setString(3, user);
+
+            prepStatement.execute();
+            success = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return success;
+    }
+}
